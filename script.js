a622da4a5700f687fc370d1dd75c04c25ccaf16f
MoMoMo7317 <mobinly7317@gmail.com>
2024-12-16 09:30:08 +0700

    new test_v3

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 console.log("warunya boonkeng");
 console.log("mo");
+
+// Select the display element
+const display = document.getElementById('display');
+
+// Function to append value to the display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to delete the last character from the display
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Function to calculate the result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
